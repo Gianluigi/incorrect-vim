d2fdc8d78fa68c07cdbdb84caaa3b1e99432202f
Gianluigi Rubino <gianluigi@grsoft.org>
2014-05-09 02:47:57 +0200

    Install script now installs plugins via vundle from the command line

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #|/bin/sh
 
+# get the path of the running script 
+# use it for building the vim runtime path 
+# the install scripts makes no assumption on the directory name
+# for your VIM configuration
 SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P)
-echo $SCRIPT_PATH
+
 # copy .vimrc and .gvimrc to the home
 echo "set rtp+=$SCRIPT_PATH" > ~/.vimrc
+echo "set rtp+=$SCRIPT_PATH/bundle/vundle/" >> ~/.vimrc
 echo "source $SCRIPT_PATH/vimrc" >> ~/.vimrc
 cp $SCRIPT_PATH/gvimrc ~/.gvimrc
+
+# install plugins with vundle
+vim +PluginInstall +qall
