c84cc3c133296c5487b8f13a70b25b100862e2ab
Gianluigi Rubino <gianluigi@grsoft.org>
2016-04-04 20:36:25 +0000

    fix installation of vundle using git

--- install.sh
+++ install.sh
@@ -12,5 +12,6 @@ echo "set rtp+=$SCRIPT_PATH/bundle/vundle/" >> ~/.vimrc
 echo "source $SCRIPT_PATH/vimrc" >> ~/.vimrc
 cp $SCRIPT_PATH/gvimrc ~/.gvimrc
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~$SCRIPT_PATH/bundle/vundle
 # install plugins with vundle via command line
 vim +PluginInstall +qall
